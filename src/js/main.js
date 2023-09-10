0c77c87bdc735652e5cb8d666572e0cc27f6ade5
patryk-pi <patryk.piasecki1@gmail.com>
2023-09-10 22:22:31 +0200

    removed commentars from main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
-// Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
 
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
-import * as app from './app.ts'
+import * as app from "./app.ts";
